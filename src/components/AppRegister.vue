6588f694e9b6c8c80b309749594b1250165c7c3d
Adélia Gonçalves <>
2018-12-05 18:56:32 +0000

    register_2

--- src/components/AppRegister.vue
+++ src/components/AppRegister.vue
@@ -43,11 +43,52 @@
 
 <script>
     import AppFooter from './AppFooter'
+    import axios from 'axios'
     
     export default {
         components: {
             AppFooter
         },
     
+    
+  
+    data: () => ({
+      valid: true,
+      name: '',
+      nameRules: [
+        v => !!v || 'Name is required',
+        v => (v && v.length <= 10) || 'Name must be less than 10 characters'
+      ],
+      email: '',
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /.+@.+/.test(v) || 'E-mail must be valid'
+      ],
+      select: null,
+      items: [
+        'Item 1',
+        'Item 2',
+        'Item 3',
+        'Item 4'
+      ],
+      checkbox: false
+    }),
+
+    methods: {
+      submit () {
+        if (this.$refs.form.validate()) {
+          // Native form submission is not yet supported
+          axios.post('/api/submit', {
+            name: this.name,
+            email: this.email,
+            select: this.select,
+            checkbox: this.checkbox
+          })
+        }
+      },
+      clear () {
+        this.$refs.form.reset()
+      }
     }
+  }
 </script>
